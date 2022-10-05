d2d2a2144d09fb3ab7feaeba56ff2fb0ad3254c4
pulko <ftkachenko.work@gmail.com>
2022-10-06 00:19:42 +0200

    refa: heading naming

--- components/post/more-posts.js
+++ components/post/more-posts.js
@@ -5,8 +5,8 @@ const MorePosts = (props) => {
 
   return (
     <section>
-      <h3 className="mb-8 text-7xl font-bold tracking-tighter leading-tight">
-        More Stories
+      <h3 className="mb-8 text-4xl font-bold tracking-tighter leading-tight text-center">
+        More Posts
       </h3>
 
       <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
